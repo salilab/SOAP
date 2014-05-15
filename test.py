c488febd8f09348ed99f088895c31bde6b0251ac
gqdong <gqdong@gmail.com>
2014-05-15 15:39:24 -0700

    add mhc2 example files

--- test.py
+++ test.py
@@ -3,6 +3,23 @@
 
 """
 
-from env import *
-from test import *
-from recoveryFunction import *
+class TestMHC2(unittest.TestCase):
+
+    def setUp(self):
+        pass
+        self.seq = range(10)
+
+    def test_decoys_preparation(self):
+        # make sure the shuffled sequence does not lose any elements
+        random.shuffle(self.seq)
+        self.seq.sort()
+        self.assertEqual(self.seq, range(10))
+
+    def test_model_selection(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
