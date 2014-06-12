2edc870015b2d155275998c7aa1330f57b139e29
gqdong <gqdong@gmail.com>
2014-06-12 11:09:20 -0700

    more documentation

--- envConfig.py
+++ envConfig.py
@@ -27,6 +27,12 @@ class SOAPenv(object):
         self.sgeServer=''#for submitting jobs on sge cluster, ssh key authencation needed for passwordless access both way (local host<->server)
         self.userName="" # username on local host and sge server, must use RSA Key for passwordless SSH Authentication
         self.localStatPotPath=''# the root path of statistical potential files in local host (could take TB of spaces)
+        .. note::
+            the statpot directory must contains:
+            #. file rsn with a 5-6 digit number in it, eg. 10000
+            #. folder results for storing optimization results
+            #. folder Decoys for storing decoys metadata used during training
+            #. folder pdbpir for storing the filtered pir sequence files for PDB structures 
         self.localDecoysPath='' # the directory for storing decoys on local host
         self.localProcessedPDBPaths=['',''] # paths to preprocessed pdb files on local host
         self.serverProcessedPDBPaths=['',''] # paths to processed pdb files on sge server
@@ -39,9 +45,11 @@ class SOAPenv(object):
         self.serverInstallPath='' # path to SOAP module on sge server
         self.serverScrathPath=''# for storing temporary results on sge cluster
         self.localRunCachePath='' # temporary run path on local host
-        self.localResultDB='x.sql3'  # database file location on local host    
-
-    **All paths defined in this file should exist.**"""
+        self.localResultDB='x.sql3'  # database file location on local host
+        
+    .. warning::
+        All paths defined in this file should exist.
+    """
     
     
     def __init__(self):
