f5e8a1e03287dcbc3c94a19eb92b95da980dc350
Ben Webb <ben@salilab.org>
2014-06-26 17:16:48 -0700

    Fix intersphinx link.

--- doc/conf.py
+++ doc/conf.py
@@ -244,8 +244,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
-
+intersphinx_mapping = {'python': ('https://docs.python.org/2.7', None)}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
